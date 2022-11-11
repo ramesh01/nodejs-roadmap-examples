cff1bab2f94a89f5863a9a16b42ca7f29d503847
Your Name <>
2022-11-11 20:57:29 +0530

    Feat: Filesystem added

---
+++ docs/filesystem/filesystem.js
@@ -0,0 +1,45 @@
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const fileOp = async () => {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, 'newsample.txt'), 'utf8');
+        await fsPromises.writeFile(path.join(__dirname, 'newsample.txt'), 'Fuck my life');
+        // delete file
+        // await fsPromises.unlink(path.join(__dirname, 'sample.txt'));
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fileOp();
+
+// Read data
+// fs.readFile(path.join(__dirname, 'sample.txt'), 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+// process.on('uncaughtException', err=> {
+//     console.error(`There was an uncaught error: ${err}`)
+//     process.exit(1);
+// });
+
+// // Write data
+// fs.writeFile(path.join(__dirname, 'sample.txt'), 'Fuck my life', (err) => {
+//     if (err) throw err;
+
+//     // Append data
+//     fs.appendFile(path.join(__dirname, 'sample.txt'), 'Testing life', (err) => {
+//         if (err) throw err;
+//         console.log("append complete");
+//     });
+
+//     // rename file name
+//     fs.rename(path.join(__dirname, 'sample.txt'), path.join(__dirname, 'newsample.txt'), (err) => {
+//         if (err) throw err;
+//         console.log("rename complete");
+//     });
+// });
+
---
+++ docs/filesystem/newsample.txt
@@ -0,0 +1,1 @@
+Fuck my life
